368dbbefdf05f4cb166d3ffffacbfb8af7401c53
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2025-01-23 12:17:52 +0100

    Update auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,9 +1,55 @@
-exports.authorizeRole = (role) => {
-    return (req, res, next) => {
-      const userRole = req.session.userRole; // Récupérer le rôle depuis la session
-      if (userRole !== role) {
-        return res.status(403).send({ message: 'Accès interdit' });
-      }
-      next();
-    };
-  };
+// verifie si l'utilisateur est un village
+
+const isVillage = (req, res, next) => {
+  const user = req.user;
+  if (user.type !== 'village') {
+    return res.status(403).send({ message: 'Vous ne pouvez pas accéder à ces informations' });
+  }
+  next();
+};
+
+// verifie si l'utilisateur est un pas
+    const isPas = (req, res, next) => {
+  const user = req.user;
+  if (user.type !== 'pas') {
+    return res.status(403).send({ message: 'Vous ne pouvez pas accéder à ces informations' });
+  }
+  next();
+};
+
+// verifie si l'utilisateur est un daader
+
+const isDaader = (req, res, next) => {
+  const user = req.user;
+  if (user.type !== 'daader') {
+    return res.status(403).send({ message: 'Vous ne pouvez pas accéder à ces informations' });
+  }
+  next();
+};
+
+// verifie si l'utilisateur est un drader
+
+const isDrader = (req, res, next) => {
+  const user = req.user;
+  if (user.type !== 'drader') {
+    return res.status(403).send({ message: 'Vous ne pouvez pas accéder à ces informations' });
+  }
+  next();
+}
+
+// verifie si l'utilisateur est un ddader
+const isDdader = (req, res, next) => {
+  const user = req.user;
+  if (user.type !== 'ddader') {
+    return res.status(403).send({ message: 'Vous ne pouvez pas accéder à ces informations' });
+  }
+  next();
+}
+
+module.exports = {
+  isVillage,
+  isPas,
+  isDaader,
+  isDrader,
+  isDdader
+}
