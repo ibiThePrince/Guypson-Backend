6f0e10961309e649cf7b5b6fea1da88cf06da5d5
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2025-01-23 12:18:12 +0100

    Update reporting.routes.js

--- routes/reporting/reporting.routes.js
+++ routes/reporting/reporting.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ReportController = require('../../controllers/Reporting/reporting.controller');
+const { isDdader } = require('../../middleware/auth.middleware');
 
 // Routes Producteurs
-router.get('/:speculation/:debut/:fin/:village', ReportController.getReport);
-
+router.get('/ddader',isDdader, ReportController.getDdaderByDrader);
+router.get('/daader', ReportController.getDaaderByDdader)
+router.get('/pas', ReportController.getPasByDaader)
+router.get('/village', ReportController.getVillageByPas)
+router.get('/campages/:id_speculation', ReportController.getCampagneBySpecAndVillage)
 
 module.exports = router;
